088fc0bb533650c8266aa158a5700bdfe11f194b
Carolina Villalobos <carito15villalobos@gmail.com>
2022-11-04 15:47:19 -0400

    typeorm heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,16 @@ import { OrderDetailModule } from './order-detail/order-detail.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+       host: 'us-cdbr-east-06.cleardb.net',
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'bs-api',
+      username: 'b167f3cfab4e67',
+      password: '400541f8 ',
+      database: 'heroku_8c5feadb42c0d74',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'root',
+      // database: 'bs-api',
       entities: ['dist/**/**.entity{.ts,.js}'],
       //entities: [Role,User],//not work in web app
       synchronize: true,
